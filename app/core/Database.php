d265887b2bf08bffcbd1b7285a241d8e8eeda62a
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-05 17:11:36 +0100

    added Database helper to connect to the db

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,12 +1,25 @@
 <?php
-class Database {
-    private static PDO $instance;
+require_once(__DIR__ . "/../core/Console.php");
+class Database
+{
 
-    public static function getInstance(): PDO {
+    public static function getInstance(): PDO
+    {
         try {
-            
-        } catch (PDOException) {
+            return new PDO(
+                "mysql:host=bdd;dbname=allo-cine;charset=utf8",
+                "root",
+                "root",
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+            );
+
+        } catch (PDOException $e) {
+            console("Erreur de connexion à la BDD : " . $e->getMessage());
             exit();
         }
+
     }
 }
