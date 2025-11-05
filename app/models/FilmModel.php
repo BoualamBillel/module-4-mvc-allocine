25d5dfa9f895cfab78b37e33316ea97009e38f13
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-05 16:35:58 +0100

    Created FilmEntity

---
+++ app/models/FilmModel.php
@@ -0,0 +1,13 @@
+<?php
+class FilmModel {
+
+}
+
+class FilmEntity {
+    private int $id;
+    private string $nom;
+    private DateTime $date_sortie;
+    private string $genre;
+    private string $realisateur;
+    private int $duree;
+}
