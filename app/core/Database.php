f837844f64d2540ac35633b054ab8b1a8dad42a8
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-06 09:50:50 +0100

    Database now uses singleton pattern

--- app/core/Database.php
+++ app/core/Database.php
@@ -3,23 +3,33 @@ require_once(__DIR__ . "/../core/Console.php");
 class Database
 {
 
+
+    private function __construct()
+    {
+    }
+
+    private static ?PDO $instance = null;
+
     public static function getInstance(): PDO
     {
-        try {
-            return new PDO(
-                "mysql:host=bdd;dbname=allo-cine;charset=utf8",
-                "root",
-                "root",
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ]
-            );
-
-        } catch (PDOException $e) {
-            console("Erreur de connexion à la BDD : " . $e->getMessage());
-            exit();
+        if (self::$instance === null) {
+            try {
+                self::$instance = new PDO(
+                    "mysql:host=bdd;dbname=allo-cine;charset=utf8",
+                    "root",
+                    "root",
+                    [
+                        PDO::ATTR_ERRMODE => pdo::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+                console("Connexion BDD Crée");
+            } catch (PDOException $e) {
+                console("Erreur de connexion à la BDD : " . $e->getMessage());
+                exit();
+            }
         }
+        return self::$instance;
 
     }
 }
