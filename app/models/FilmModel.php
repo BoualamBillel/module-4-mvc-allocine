9aa9c8a13c0c00389e5e9e25b57b74e9c2f25eb1
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-06 11:08:09 +0100

    added get_all_films and add_film method, created a new type ENUM to match the genre allowed in enum genre in the sql table

--- app/models/FilmModel.php
+++ app/models/FilmModel.php
@@ -1,25 +1,100 @@
 <?php
 require_once(__DIR__ . "/../core/Console.php");
 require_once(__DIR__ . "/../core/Database.php");
+
+// Creation du type ENUM
+enum GENRE_ALLOWED
+{
+    case Action;
+    case Comedie;
+    case Drama;
+    case Fantaisie;
+    case Horreur;
+    case Documentaire;
+    case ScienceFiction;
+    case Autre;
+}
 class FilmModel
 {
     private PDO $bdd;
+    private PDOStatement $add_film;
+    private PDOStatement $get_all_films;
 
 
     function __construct()
     {
+        // Instanciation de la BDD
         $this->bdd = Database::getInstance();
+        // Préparations des requetes préparés
+        $this->add_film = $this->bdd->prepare("INSERT INTO `Film`(nom, date, genre,  realisateur, duree) VALUES (:nom, :date, :genre, :realisateur, :duree)");
+        $this->get_all_films = $this->bdd->prepare("SELECT * FROM `Film LIMIT :limit");
     }
 
+    public function add_film(string $nom, DateTime $date, string $genre, string $realisateur, int $duree)
+    {
+        // TODO - Verifier si le film n'existe pas deja dans la BDD
+        try {
+            $this->add_film->bindValue(":nom", $nom);
+            $this->add_film->bindValue(":date", $date->format('Y-m-d'));
+            $this->add_film->bindValue(":genre", $genre);
+            $this->add_film->bindValue(":realisateur", $realisateur);
+            $this->add_film->bindValue(":duree", $duree);
+            $this->add_film->execute();
+
+            // Message de succès
+            console("Film ajouté avec succès : $nom ($genre, $duree min)");
+        } catch (PDOException $e) {
+            // Message d'erreur
+            console("Erreur lors de l'ajout d'un film dans la BDD : " . $e->getMessage());
+            exit();
+        }
+    }
+
+    public function get_all_films(int $limit): array
+    {
+        if ($limit <= 0) {
+            console("La limite de récupération de films présent dans la BDD ne peut pas etre égale ou inférieure à 0");
+            exit();
+        }
+
+        try {
+            $this->get_all_films->bindValue(":limit", $limit);
+            $this->get_all_films->execute();
+            $raw_films = $this->get_all_films->fetchAll();
+        } catch (PDOException $e) {
+            console("Erreur SQL lors de la récupération des films de la BDD : " . $e->getMessage());
+            exit();
+        }
+
+        if (count($raw_films) <= 0) {
+            console("Aucun film présent dans la BDD");
+            exit();
+        } else {
+            // Formater la réponse dans un tableau
+            $filmEntity = [];
+            foreach ($raw_films as $film) {
+                $filmEntity[] = new FilmEntity(
+                    $film['id'],
+                    $film['nom'],
+                    $film['date'],
+                    $film['genre'],
+                    $film['realisateur'],
+                    $film['duree']
+                );
+            }
+            return $filmEntity;
+        }
+    }
 
 }
+
 
 class FilmEntity
 {
     private ?int $id = null;
     private string $nom;
     private DateTime $date_sortie;
-    private string $genre;
+    private GENRE_ALLOWED $genre;
     private string $realisateur;
     private int $duree;
 
@@ -29,16 +104,6 @@ class FilmEntity
 
     private const DUREE_MIN = 10;
 
-    private const GENRE_ALLOWED = [
-        'Action',
-        'Comedie',
-        'Drama',
-        'Fantaisie',
-        'Horreur',
-        'Documentaire',
-        'Science-Fiction',
-        'Autre'
-    ];
 
     //Getter
     public function getId(): ?int
@@ -56,7 +121,7 @@ class FilmEntity
         return $this->date_sortie;
     }
 
-    public function getGenre(): string
+    public function getGenre(): GENRE_ALLOWED
     {
         return $this->genre;
     }
@@ -97,13 +162,14 @@ class FilmEntity
         $this->date_sortie = $date;
     }
 
-    public function setGenre(string $genre)
+    public function setGenre(GENRE_ALLOWED $genre)
     {
-        if (!in_array($genre, $this::GENRE_ALLOWED)) {
-            throw new Error("Genre invalide, les genres acceptés sont : " . implode(", ", $this::GENRE_ALLOWED));
-        } else {
-            $this->genre = $genre;
+        // Verifie la valeur si la valeur de $genre fait des partie des cases presentes dans l'enumération de genre autorisés
+        if (!in_array($genre, GENRE_ALLOWED::cases())) {
+            console("Erreur : genre non autorisé (" . $genre->name . ")");
+            throw new Error("Genre invalide, les genres acceptés sont : " . implode(", ", array_map(fn($g) => $g->name, GENRE_ALLOWED::cases())));
         }
+        $this->genre = $genre;
     }
 
     public function setRealisateur(string $realisateur)
@@ -124,7 +190,7 @@ class FilmEntity
         }
     }
 
-    public function __construct(?int $id = null ,string $nom, DateTime $dateDeSortie, string $genre, string $realisateur, int $duree)
+    public function __construct(?int $id = null, string $nom, DateTime $dateDeSortie, GENRE_ALLOWED $genre, string $realisateur, int $duree)
     {
         $this->id = $id;
         $this->setNom($nom);
