a2ce8816c8f3be9a2899d8e80c03b0801b20485e
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-07 09:14:23 +0100

    router done

---
+++ app/core/Router.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__ . "/../controllers/FilmController.php");
+
+class Router {
+    public static function getController(string $controllerName) {
+        switch ($controllerName) {
+            // Route /film
+            case 'film' :
+                return new FilmController();
+        }
+    }
+}
