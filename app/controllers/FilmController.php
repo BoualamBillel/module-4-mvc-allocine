b061e2c952cdb9617f943d9f6b811c0a7c745f0c
Billel Boualam <boualambillelsofien@gmail.com>
2025-11-08 14:05:24 +0100

    The JSONAll() method was implemented to return an array of JSON objects containing all the movies in the database.

--- app/controllers/FilmController.php
+++ app/controllers/FilmController.php
@@ -2,13 +2,14 @@
 require_once(__DIR__ . "/../models/FilmModel.php");
 class FilmController
 {
-    public function view(string $method, array $params = []){
+    public function view(string $method, array $params = [])
+    {
         try {
             call_user_func([$this, $method], $params);
         } catch (Error $e) {
             console($e->getMessage());
         }
-    }   
+    }
     public function index()
     {
         $filmModel = new FilmModel();
@@ -25,5 +26,25 @@ class FilmController
 
         //Affichage dans la vue
         require_once(__DIR__ . "/../views/film.php");
+    }
+
+    public function jsonall($params = [])
+    {
+
+        $filmModel = new FilmModel();
+        $films = $filmModel->get_all_films();
+        $filmsArray = array_map(function($film) {
+            return [
+                'nom' => $film->getNom(),
+                'genre' => $film->getGenre()->value,
+                'duree' => $film->getDuree(),
+                'realisateur' => $film->getRealisateur(),
+                'date' => $film->getDateDeSortie()->format("d/m/Y")
+            ];
+        },$films);
+
+        header("Content-type: application/json");
+        echo json_encode($filmsArray);
+
     }
 }
